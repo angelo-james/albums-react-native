b2eae86ddd26896aa44debf7aa7d025d0e8634cf
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-30 12:14:32 -0800

    imported albumdetail component and created renderAlbums funtion to map through albbums data and pass props tos album detail component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = {
@@ -14,13 +15,16 @@ class AlbumList extends Component {
     }))
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <AlbumDetail key={album.title} album={album}/>
+    )
+  }
+
   render() {
-    console.log(this.state)
     return (
       <View>
-        <Text>
-          Album List
-        </Text>
+        {this.renderAlbums()}
       </View>
     )
   }
